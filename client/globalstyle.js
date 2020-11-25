339a62c51735a4eb6c5651d37f0801a7f52b9e5d
Frenzy265 <klaus_franziska@gmx.de>
2020-11-25 15:47:02 +0100

    💄 Add global style

---
+++ client/globalstyle.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components/makro";
+
+const GlobalStyle = createGlobalStyle`
+*,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+:root {
+    --main-color: #264031;
+    --secondary-color: #778C80;
+    --action-color: #F2CB05;
+    --noaction-color: #fff;
+    --light-font-color: #fff;
+    --dark-font-color: var(----main-color)
+}
+
+body {
+    margin: 0;
+    background: var(----main-color);
+    font-family: "Roboto", sans-serif;
+}
+
+h1 {
+    color: var(----main-color);
+    font-size: 35px;
+    font-family: "Permanent Marker", sans-serif;
+}
+
+h2 {
+    color: var(----main-color);
+    font-size: 20px;
+    font-weight: bold;
+}
+
+
+`;
+
+export default GlobalStyle;
