95b055c7947c56acf2f2da04fe95059b37d20458
Frenzy265 <klaus_franziska@gmx.de>
2020-11-26 13:48:56 +0100

    💄 Add header and stories

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/macro";
+
+const Header = styled.header`
+  display: grid;
+  font-family: var(--header-font);
+  place-items: center;
+  min-height: 100px;
+  font-size: 35px;
+  word-wrap: break-word;
+  line-height: 30px;
+  text-align: center;
+`;
+
+export default Header;
