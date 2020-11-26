58e28d873bd56ebe026a85c18bf8de51010069f9
Frenzy265 <klaus_franziska@gmx.de>
2020-11-26 17:06:56 +0100

    💄 Add button and stories

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,56 +1,33 @@
-import React from "react";
+import styled from "styled-components/macro";
 import PropTypes from "prop-types";
-import "./button.css";
 
-/**
- * Primary UI component for user interaction
- */
-const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
-  return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+const Button = styled.button`
+  background-color: ${(props) =>
+    props.active ? "var(--action-color)" : "var(--secondary-color)"};
+  color: var(--main-color);
+  height: 50px;
+  min-width: 300px;
+  max-width: 80vw;
+  border-radius: 7px;
+  border: none;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.16);
+  text-align: left;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    height: 20px;
+    margin-right: 20px;
+  }
+
+  p {
+    margin-left: 20px;
+  }
+`;
 
 Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: "medium",
-  onClick: undefined,
+  active: PropTypes.bool,
 };
 
 export default Button;
