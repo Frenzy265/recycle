fc59de6b9279c53eb4c6f8b52e935a6cea0460ab
Frenzy265 <klaus_franziska@gmx.de>
2020-11-30 16:08:22 +0100

    Add Pagecontainer to app.js

---
+++ client/src/components/PageContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+
+const PageContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  padding: 15px;
+`;
+
+export default PageContainer;
