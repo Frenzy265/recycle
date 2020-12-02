cd29cd199a28f0306b14a3f3933dc359028b957f
Frenzy265 <klaus_franziska@gmx.de>
2020-12-02 16:27:42 +0100

    ✨ Add I18n context

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,34 @@ import BoxStart from "./pages/BoxStart";
 import SearchResult from "./pages/SearchResult";
 import SearchStart from "./pages/SearchStart";
 import Home from "./pages/Home";
+import { I18nContext } from "./context/i18n";
+import { useState } from "react";
+
+const localeDicts = {
+  de: {
+    content: "Hier steht die deutsche Übersetzung",
+  },
+  en: {
+    content:
+      "The 3 Rs Reduce, Reuse, Recycle – these three R words are an important part of sustainable living, as they help to cut down on the amount of waste we have to throw away. Its Really simple! Reduce the amount of waste you produce. Reuse items as much as you can before replacing them. Recycle items wherever possible. Using the 3 Rs also helps to minimise the amount of space needed for landfill sites, where waste materials are disposed of.",
+  },
+};
 
 function App() {
+  const [dict, setDict] = useState(localeDicts.de);
+
   return (
-    <>
+    <I18nContext.Provider value={dict}>
       <Router>
         <GlobalStyle />
         <PageContainer>
           <Headline />
+          <select
+            onChange={(event) => setDict(localeDicts[event.target.value])}
+          >
+            <option value="de">DE</option>
+            <option value="en">EN</option>
+          </select>
           <Switch>
             <Route exact path="/">
               <Home />
@@ -45,7 +65,7 @@ function App() {
           <Navbar />
         </PageContainer>
       </Router>
-    </>
+    </I18nContext.Provider>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,12 @@
+import { useContext } from "react";
 import TextField from "../components/TextField";
+import { I18nContext } from "../context/i18n";
 
 export default function Home() {
+  const dict = useContext(I18nContext);
   return (
     <>
-      <TextField>
-        The 3 Rs Reduce, Reuse, Recycle – these three R words are an important
-        part of sustainable living, as they help to cut down on the amount of
-        waste we have to throw away. Its Really simple! Reduce the amount of
-        waste you produce. Reuse items as much as you can before replacing them.
-        Recycle items wherever possible. Using the 3 Rs also helps to minimise
-        the amount of space needed for landfill sites, where waste materials are
-        disposed of.
-      </TextField>
+      <TextField>{dict.content}</TextField>
     </>
   );
 }
