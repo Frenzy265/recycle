40e218a57592b82feb3d0888d8c15447f4dbbddd
Frenzy265 <klaus_franziska@gmx.de>
2020-12-02 10:55:27 +0100

    ♻️ Update page gtknow with new component textfield

--- client/src/pages/GoodToKnow.js
+++ client/src/pages/GoodToKnow.js
@@ -1,15 +1,9 @@
-import styled from "styled-components/macro";
-
-const Text = styled.p`
-  color: var(--tertiary-color);
-  text-align: center;
-  margin: 0 20px;
-`;
+import TextField from "../components/TextField";
 
 export default function GoodToKnow() {
   return (
     <>
-      <Text>
+      <TextField>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -19,7 +13,7 @@ export default function GoodToKnow() {
         sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
         rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
         ipsum dolor sit amet.
-      </Text>
+      </TextField>
     </>
   );
 }
