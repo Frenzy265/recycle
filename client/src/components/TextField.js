ec5a6888067d05dad94ecd5b9b1cf7793f8c3ae2
Frenzy265 <klaus_franziska@gmx.de>
2020-12-02 10:54:29 +0100

    ♻️  create component textfield

---
+++ client/src/components/TextField.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+
+const TextField = styled.p`
+  color: var(--tertiary-color);
+  text-align: center;
+  margin: 0 20px;
+`;
+
+export default TextField;
