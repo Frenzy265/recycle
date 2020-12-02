03c10be40993b1ed83e679040035112cfa2cec09
Frenzy265 <klaus_franziska@gmx.de>
2020-12-02 11:06:12 +0100

    ✨ Add home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import TextField from "../components/TextField";
+
+export default function Home() {
+  return (
+    <>
+      <TextField>
+        The 3 Rs Reduce, Reuse, Recycle – these three R words are an important
+        part of sustainable living, as they help to cut down on the amount of
+        waste we have to throw away. Its Really simple! Reduce the amount of
+        waste you produce. Reuse items as much as you can before replacing them.
+        Recycle items wherever possible. Using the 3 Rs also helps to minimise
+        the amount of space needed for landfill sites, where waste materials are
+        disposed of.
+      </TextField>
+    </>
+  );
+}
