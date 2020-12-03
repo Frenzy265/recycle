69d4acb8e3afb313ad50ff8d4dffaebfd43eca02
Frenzy265 <klaus_franziska@gmx.de>
2020-12-03 12:18:27 +0100

    ✨ Get boxes from api

--- client/src/pages/BoxStart.js
+++ client/src/pages/BoxStart.js
@@ -3,6 +3,8 @@ import IconBoxOld from "../assets/icon-box-primary.svg";
 import IconBoxNew from "../assets/icon-box-new-primary.svg";
 import styled from "styled-components/macro";
 import { Link } from "react-router-dom";
+import { getBoxes } from "../utils/api-boxes";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -11,6 +13,13 @@ const Container = styled.div`
 `;
 
 export default function BoxStart() {
+  const [boxes, setBoxes] = useState(null);
+
+  useEffect(async () => {
+    const existBoxes = await getBoxes();
+    setBoxes(existBoxes);
+  }, []);
+
   return (
     <Container>
       <Link to="/box/new">
@@ -19,18 +28,12 @@ export default function BoxStart() {
           <p>Neue Kiste</p>
         </Box>
       </Link>
-      <Box existing>
-        <img src={IconBoxOld} alt="Icon Box" />
-        <p>Wertstoffhof</p>
-      </Box>
-      <Box existing>
-        <img src={IconBoxOld} alt="Icon Box" />
-        <p>ebay</p>
-      </Box>
-      <Box existing>
-        <img src={IconBoxOld} alt="Icon Box" />
-        <p>Kleidercontainer</p>
-      </Box>
+      {boxes?.map((box) => (
+        <Box key={box.id} existing>
+          <img src={IconBoxOld} alt="Icon Box" />
+          <p>{box.title}</p>
+        </Box>
+      ))}
     </Container>
   );
 }
