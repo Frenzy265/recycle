2dd390093ceda5a59b77adc3b7f40814198facbb
Frenzy265 <klaus_franziska@gmx.de>
2020-12-04 17:46:28 +0100

    Update api calls

--- client/src/utils/api-boxes.js
+++ client/src/utils/api-boxes.js
@@ -1,30 +1,28 @@
-//tested and is working - at page BoxStart :-)
 export async function getBoxes() {
   const response = await fetch("http://localhost:5000/boxes");
   const boxes = await response.json();
   return boxes;
 }
 
-export async function getBoxById(id) {
-  const response = await fetch(`http://localhost:5000/boxes/${id}`);
+export async function getBoxByTitle(title) {
+  const response = await fetch(`http://localhost:5000/boxes?title=${title}`);
   const box = await response.json();
-  return box;
+  console.log(box);
+  return box[0];
 }
 
-export async function deleteBoxById(id) {
-  await fetch(`http://localhost:5000/boxes/${id}`, { method: "DELETE" });
-}
-
-export async function deleteItemByName(id, name) {
-  await fetch(`http://localhost:500/boxes/${id}/${name}`, { method: "DELETE" });
-}
-
-export async function postBox(box) {
-  const response = await fetch("http://localhost:5000/boxes", {
+export async function postBoxById(box) {
+  const response = await fetch(`http://localhost:5000/boxes`, {
     method: "POST",
-    header: { "content-Type": "application/json" },
     body: JSON.stringify(box),
+    headers: { "Content-Type": "application/json" },
   });
   const newBox = await response.json();
   return newBox;
+}
+
+export async function deleteBoxById(id) {
+  await fetch(`http://localhost:5000/boxes/${id}`, {
+    method: "DELETE",
+  });
 }
