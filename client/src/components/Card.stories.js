581d700589e159272a5a3a880070f03edc0d9cda
Frenzy265 <klaus_franziska@gmx.de>
2020-12-14 17:49:29 +0100

    ⚡️ Add props to storie

--- client/src/components/Card.stories.js
+++ client/src/components/Card.stories.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Card from "./Card";
+import { Card } from "./Card";
+import IconRecycle from "../assets/icon-recycle-action.svg";
+import IconBox from "../assets/icon-box-action.svg";
 
 export default {
   title: "Components/Card",
@@ -7,4 +9,22 @@ export default {
   component: Card,
 };
 
-export const card = () => <Card />;
+const Template = (args) => <Card {...args} />;
+
+export const Introduction = Template.bind({});
+Introduction.args = {
+  title: "Reduce",
+  icon: IconRecycle,
+  alt: "Icon Recycle",
+  infoOne:
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
+};
+
+export const Overview = Template.bind({});
+Overview.args = {
+  title: "Recyclingkisten",
+  icon: IconBox,
+  alt: "Icon Box",
+  infoOne: "Offene Boxen: 5",
+  infoTwo: "Geschlossene Boxen: 10",
+};
