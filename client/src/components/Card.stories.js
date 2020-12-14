0f73eaa507838afb4a1ff61bda709d9a093b1260
Frenzy265 <klaus_franziska@gmx.de>
2020-12-14 15:57:59 +0100

    Add stories

---
+++ client/src/components/Card.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from "./Card";
+
+export default {
+  title: "Components/Card",
+  parameters: { layout: "centered" },
+  component: Card,
+};
+
+export const card = () => <Card />;
