38920d715c53469d3fd97ad4b5d154a503725f24
Frenzy265 <klaus_franziska@gmx.de>
2020-12-14 15:56:44 +0100

    Update naming of component

--- client/src/components/Input.stories.js
+++ client/src/components/Input.stories.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Inputfield } from "./Input";
+import { InputField } from "./Input";
 import IconAdd from "../assets/icon-add-primary.svg";
 import IconSearch from "../assets/icon-search-primary.svg";
 
 export default {
   title: "Components/Inputfield",
   parameters: { layout: "centered" },
-  component: Inputfield,
+  component: InputField,
 };
 
-const Template = (args) => <Inputfield {...args} />;
+const Template = (args) => <InputField {...args} />;
 
 export const NewItem = Template.bind({});
 NewItem.args = {
