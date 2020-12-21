4b884230e23520ef7f7a537a3d18b98aa057bb4e
Frenzy265 <klaus_franziska@gmx.de>
2020-12-21 12:59:48 +0100

    🐛 Change width of item and link

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -8,7 +8,8 @@ const ListItem = styled.li`
   background-color: var(--secondary-color);
   color: var(--primary-color);
   justify-content: space-between;
-  margin: 0.25rem 0;
+  margin: 0.25rem auto;
+  max-width: 300px;
 
   img {
     height: 20px;
