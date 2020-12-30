70bcc030d3820e986426db52c05815e74006efa2
Frenzy265 <klaus_franziska@gmx.de>
2020-12-30 14:12:40 +0100

    ✨ Add BackButton on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,10 @@
+import React from "react";
 import styled from "styled-components/macro";
+import { useLocation } from "react-router-dom";
+import { BackButton } from "../components/BackButton";
+import PropTypes from "prop-types";
 
-const Header = styled.header`
+const Headline = styled.header`
   display: grid;
   font-family: var(--header-font);
   color: var(--action-color);
@@ -12,6 +16,25 @@ const Header = styled.header`
   text-align: center;
   width: 100%;
   background-color: var(--primary-color);
+  grid-template-columns: 20% auto 20%;
 `;
 
+const Header = ({ title }) => {
+  const location = useLocation();
+
+  return (
+    <>
+      <Headline>
+        <div> {location.pathname !== "/" && <BackButton />}</div>
+        {title}
+        <div></div>
+      </Headline>
+    </>
+  );
+};
+
 export default Header;
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
