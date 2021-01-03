6d287e201fdeda3cbf0132c6f8fc10166fd79396
Frenzy265 <klaus_franziska@gmx.de>
2021-01-03 15:20:36 +0100

    💄 Add storie headerbackbutton

---
+++ client/src/components/HeaderBackButton.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderBackButton } from "./HeaderBackButton";
+
+export default {
+  title: "Components/Headerbackbutton",
+  parameters: { layout: "centered" },
+  component: HeaderBackButton,
+};
+
+export const headerbackButton = () => (
+  <HeaderBackButton>Header</HeaderBackButton>
+);
