470f288f30465784fcc8f00c1f88f4d87536e20c
Frenzy265 <klaus_franziska@gmx.de>
2021-01-05 10:46:11 +0100

    ⚡️ Change body input and add charset

--- client/src/api/challenge.js
+++ client/src/api/challenge.js
@@ -13,8 +13,8 @@ export async function countTasks() {
 export async function updateStatusTask(task) {
   const response = await fetch(`/api/challenge/${task._id}`, {
     method: "PATCH",
-    body: JSON.stringify(task.done),
-    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+    headers: { "Content-Type": "application/json;charset=utf-8" },
   });
   const update = await response.json();
   return update;
