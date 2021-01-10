86e90de9d9097399792f8316d9991832b0c78cbd
Frenzy265 <klaus_franziska@gmx.de>
2021-01-10 15:59:44 +0100

    💄 Redesign Splashpage

--- client/src/pages/SplashScreen.js
+++ client/src/pages/SplashScreen.js
@@ -1,13 +1,37 @@
-import Banane from "../assets/banane_cut.jpg";
+import Banane from "../assets/banane-transparent.png";
+import Typo from "../assets/typo-transparent.png";
 import styled from "styled-components/macro";
 
 const SplashContainer = styled.div`
-  display: grid;
-  place-items: center;
-  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding-left: 20px;
 
   img {
-    height: 70vh;
+    height: 50vh;
+    padding-top: 80px;
+    padding-bottom: 20px;
+  }
+`;
+const Animation = styled.img`
+  height: 30vh;
+  animation: wobble 0.6s 2;
+  animation-delay: 1s;
+
+  @keyframes wobble {
+    25% {
+      transform: rotate(15deg);
+    }
+    50% {
+      transform: rotate(-30deg);
+    }
+    75% {
+      transform: rotate(5deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
   }
 `;
 
@@ -15,8 +39,9 @@ export default function SplashScreen() {
   return (
     <>
       <SplashContainer>
-        <img src={Banane} alt="Start with Banane" />
+        <img src={Typo} alt="Claim" />
       </SplashContainer>
+      <Animation src={Banane} alt="Banane" />
     </>
   );
 }
